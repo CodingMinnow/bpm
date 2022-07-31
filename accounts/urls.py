74e8a7dec9f4dc669f1359999d98fbb0e6fedaee
smo957 <smo957@users.noreply.github.com>
2022-07-31 10:05:30 -0400

    comment out password reset for now

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path('activate/<uidb64>/<token>/', views.ActivateAccount.as_view(), name="activate"),
-    path('password reset/'),
-    path('reset/')
+    # path('password reset/'),
+    # path('reset/')
 ]
